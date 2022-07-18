75194b5299b4526d0804ae475a9bb1863526e823
EC2 Default User <ec2-user@ip-172-31-66-90.ec2.internal>
2022-07-18 08:10:09 +0000

    フラッシュメッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'books/index'
   resources :books
   get 'books/:id' => 'books#show' , as: 'bookers'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_bookers'
   get 'homes/top'
   get 'top' => 'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_bookers'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
